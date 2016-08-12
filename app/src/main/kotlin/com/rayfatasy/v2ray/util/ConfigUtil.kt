600621f361302db1e6c23ff489db14abe84a95cd
ztc1997 <2455484588@qq.com>
2016-08-12 22:24:29 +0800

    ConfigUtil.kt: replacementPairs: Use lazy initialization and reformat

--- app/src/main/kotlin/com/rayfatasy/v2ray/util/ConfigUtil.kt
+++ app/src/main/kotlin/com/rayfatasy/v2ray/util/ConfigUtil.kt
@@ -4,51 +4,54 @@ import org.json.JSONException
 import org.json.JSONObject
 
 object ConfigUtil {
-    val replacementPairs = listOf("port" to 10808,
-            "inbound" to JSONObject("""{
-        "protocol": "socks",
-        "listen": "127.0.0.1",
-        "settings": {
-            "auth": "noauth",
-            "udp": true
-        }
-    }"""),
-            "#lib2ray" to JSONObject("""{
-    "enabled": true,
-    "listener": {
-      "onUp": "#none",
-      "onDown": "#none"
-    },
-    "env": [
-      "V2RaySocksPort=10808"
-    ],
-    "render": [],
-    "escort": [],
-    "vpnservice": {
-      "Target": "${"$"}{datadir}tun2socks",
-      "Args": [
-        "--netif-ipaddr",
-        "26.26.26.2",
-        "--netif-netmask",
-        "255.255.255.0",
-        "--socks-server-addr",
-        "127.0.0.1:${"$"}V2RaySocksPort",
-        "--tunfd",
-        "3",
-        "--tunmtu",
-        "1500",
-        "--sock-path",
-        "/dev/null",
-        "--loglevel",
-        "4",
-        "--enable-udprelay"
-      ],
-      "VPNSetupArg": "m,1500 a,26.26.26.1,24 r,0.0.0.0,0 d,208.67.222.222"
+    val replacementPairs by lazy {
+        listOf("port" to 10808,
+                "inbound" to JSONObject("""{
+                    "protocol": "socks",
+                    "listen": "127.0.0.1",
+                    "settings": {
+                        "auth": "noauth",
+                        "udp": true
+                    }
+                }"""),
+                "#lib2ray" to JSONObject("""{
+                    "enabled": true,
+                    "listener": {
+                    "onUp": "#none",
+                    "onDown": "#none"
+                    },
+                    "env": [
+                    "V2RaySocksPort=10808"
+                    ],
+                    "render": [],
+                    "escort": [],
+                    "vpnservice": {
+                    "Target": "${"$"}{datadir}tun2socks",
+                    "Args": [
+                    "--netif-ipaddr",
+                    "26.26.26.2",
+                    "--netif-netmask",
+                    "255.255.255.0",
+                    "--socks-server-addr",
+                    "127.0.0.1:${"$"}V2RaySocksPort",
+                    "--tunfd",
+                    "3",
+                    "--tunmtu",
+                    "1500",
+                    "--sock-path",
+                    "/dev/null",
+                    "--loglevel",
+                    "4",
+                    "--enable-udprelay"
+                    ],
+                    "VPNSetupArg": "m,1500 a,26.26.26.1,24 r,0.0.0.0,0 d,208.67.222.222"
+                    }
+                }"""),
+                "log" to JSONObject("""{
+                    "loglevel": "warning"
+                }""")
+        )
     }
-  }"""),
-            "log" to JSONObject("""{
-    "loglevel": "warning"
-            }"""))
 
     fun validConfig(conf: String): Boolean {
         try {
