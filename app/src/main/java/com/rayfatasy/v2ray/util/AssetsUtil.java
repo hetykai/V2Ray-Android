e3d1b05c89a0eab6d78587564b696e8708a3bf12
hetykai <hetykai@gmail.com>
2016-08-16 14:45:47 +0800

    Update AssetsUtil.java
    修复在6.0以上系统无法读取内存卡文件崩溃的bug

--- app/src/main/java/com/rayfatasy/v2ray/util/AssetsUtil.java
+++ app/src/main/java/com/rayfatasy/v2ray/util/AssetsUtil.java
@@ -1,7 +1,6 @@
 package com.rayfatasy.v2ray.util;
 
 import android.content.res.AssetManager;
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -9,6 +8,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 
 public class AssetsUtil {
     public static boolean copyAssetFolder(AssetManager assetManager,
@@ -75,6 +78,34 @@ public class AssetsUtil {
         int read;
         while ((read = in.read(buffer)) != -1) {
             out.write(buffer, 0, read);
+        }
+    }
+    
+    // Storage Permissions
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    /**
+     * Checks if the app has permission to write to device storage
+     * <p>
+     * If the app does not has permission then the user will be prompted to grant permissions
+     *
+     * @param activity
+     */
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
         }
     }
 }
